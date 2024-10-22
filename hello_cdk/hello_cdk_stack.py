0f1da1055c802b512b54547ff417863e61fd0975
EC2 Default User <ec2-user@ip-172-31-30-219.ec2.internal>
2024-10-22 03:32:00 +0000

    Deploy static website using S3 bucket implementing AWS CDK

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,11 +1,13 @@
 from aws_cdk import (
     # Duration,
     Stack, 
-    aws_s3 as s3,
+    aws_s3 as s3
     # aws_sqs as sqs,
 )
 
 import aws_cdk as cdk
+import aws_cdk.aws_s3_deployment as s3deployment
+
 from constructs import Construct
 
 class HelloCdkStack(Stack):
@@ -18,7 +20,16 @@ class HelloCdkStack(Stack):
                             "MyFirstBucket", 
                             versioned=True,
                             removal_policy=cdk.RemovalPolicy.DESTROY,
-                            auto_delete_objects=True)
+                            auto_delete_objects=True,
+                            block_public_access=s3.BlockPublicAccess.BLOCK_ACLS,
+                            access_control=s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+                            public_read_access = True,
+                            website_index_document="index.html")
+        
+        deployment = s3deployment.BucketDeployment(self, "DeployWebsite", 
+                            sources=[s3deployment.Source.asset('./hello_cdk/static-website.zip')],
+                            destination_bucket=bucket)
+        
         # example resource
         # queue = sqs.Queue(
         #     self, "HelloCdkQueue",
